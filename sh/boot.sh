4ccc5bf298988a342abd55e26e7ded7d2ea58c36
linwaiwai <jiansihun@foxmail.com>
2020-11-04 10:16:17 +0800

    add install command for install process ,and fix the boot.sh for darwin to run

--- sh/boot.sh
+++ sh/boot.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
+cygwin=false
+darwin=false
+os400=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+Darwin*) darwin=true;;
+OS400*) os400=true;;
+esac
+error_exit ()
+{
+    echo "ERROR: $1 !!"
+    exit 1
+}
+[ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=$HOME/jdk/java
+[ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/usr/java
+[ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/opt/taobao/java
+[ ! -e "$JAVA_HOME/bin/java" ] && unset JAVA_HOME
+
+if [ -z "$JAVA_HOME" ]; then
+  if $darwin; then
+
+    if [ -x '/usr/libexec/java_home' ] ; then
+      export JAVA_HOME=`/usr/libexec/java_home`
+
+    elif [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
+      export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
+    fi
+  else
+    JAVA_PATH=`dirname $(readlink -f $(which javac))`
+    if [ "x$JAVA_PATH" != "x" ]; then
+      export JAVA_HOME=`dirname $JAVA_PATH 2>/dev/null`
+    fi
+  fi
+  if [ -z "$JAVA_HOME" ]; then
+        error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better!"
+  fi
+fi
 
 #进入脚本所在目录
 cd `dirname $0`
 
-#变量定义
-APOLLO_META_SERVER=http://localhost:66
-ENV=dev
-APP_NAME=fizz-gateway-community-1.1.0.jar
-APP_DEP_DIR=/data/webapps/fizz-gateway
-APP_LOG_DIR=/data/logs/fizz-gateway
-JAVA_CMD=/usr/local/java/bin/java
-PID_FILE="${APP_LOG_DIR}/tpid"
-CHECK_COUNT=3
-SERVER_IP="` ip a | egrep "brd" | grep inet | awk '{print $2}' | sed 's#/24##g'`"
+#变量定义，根据需要修改BASE_DIR位置
+# export BASE_DIR=`cd $(dirname $0)/../..; pwd`
+export BASE_DIR="/data"
+export JAVA_HOME
+export JAVA_CMD="$JAVA_HOME/bin/java"
+export APOLLO_META_SERVER=http://localhost:66
+export ENV=dev
+export APP_NAME=fizz-gateway-community-1.0.0.jar
+export APP_DEP_DIR="$BASE_DIR/webapps/fizz-gateway"
+export APP_LOG_DIR="$BASE_DIR/data/logs/fizz-gateway"
+export PID_FILE="${APP_LOG_DIR}/tpid"
+export CHECK_COUNT=3
+#export SERVER_IP="` ip a | egrep "brd" | grep inet | awk '{print $2}' | sed 's#/24##g'`"
 
 #创建应用目录
 mkdir -p ${APP_DEP_DIR}
@@ -31,9 +71,6 @@ JAVA_OPTS="-Xms256m -Xmx4096m \
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:-OmitStackTraceInFastThrow \
 -verbose:gc \
--XX:+PrintGCDetails \
--XX:+PrintGCDateStamps \
--XX:+PrintHeapAtGC \
 -Xloggc:${APP_LOG_DIR}/${START_DATE_TIME}.gc \
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:HeapDumpPath=${APP_LOG_DIR}/dump.logs \
@@ -43,23 +80,35 @@ JAVA_OPTS="-Xms256m -Xmx4096m \
 -Dio.netty.noPreferDirect=true \
 -Dio.netty.noUnsafe=true "
 
+if ! $darwin; then
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC"
+fi
+
 #进程状态标识变量，1为存在，0为不存在
 PID_FLAG=0
 
 #检查服务进程是否存在
 checktpid() {
-    TPID=`cat ${PID_FILE} | awk '{print $1}'`
-    TPID=`ps -aef | grep ${TPID} | awk '{print $2}' | grep ${TPID}`
-    if [[ ${TPID} ]]
-    then
-	      PID_FLAG=1
+    if [-d ${PID_FILE}]; then
+        TPID=`cat ${PID_FILE} | awk '{print $1}'`
+        TPID=`ps -aef | grep ${TPID} | awk '{print $2}' | grep ${TPID}`
+        if [[ ${TPID} ]]
+        then
+            PID_FLAG=1
+        else
+            PID_FLAG=0
+        fi
     else
-	      PID_FLAG=0
+        PID_FLAG=0
     fi
 }
 
 #启动服务函数
 start() {
+  m_start
+}
+
+m_start() {
     #检查进程状态
     checktpid
     if [[ ${PID_FLAG} -ne 0 ]]
@@ -69,7 +118,7 @@ start() {
         echo "starting $APP_NAME ..."
         rm -f ${PID_FILE}
         #rm -rf ${APP_LOG_DIR}/flumeES/*
-        ${JAVA_CMD} -jar ${JAVA_OPTS} -Denv=$ENV -Dapollo.meta=${APOLLO_META_SERVER} ${APP_DEP_DIR}/${APP_NAME} > ${APP_LOG_DIR}/${APP_NAME}.log 2>&1 &
+        ${JAVA_CMD} -jar ${JAVA_OPTS} $1 -Denv=$ENV -Dapollo.meta=${APOLLO_META_SERVER} ${APP_DEP_DIR}/${APP_NAME} > ${APP_LOG_DIR}/${APP_NAME}.log 2>&1 &
         echo $! > ${PID_FILE}
     fi
 }
@@ -107,9 +156,29 @@ stop() {
         else
             echo "$APP_NAME Stopped!"
         fi
-		
+
     else
         echo "warn:$APP_NAME is not runing"
+    fi
+}
+
+#安装函数
+install() {
+  m_install -Dinstall=true
+}
+
+m_install() {
+    #检查进程状态
+    checktpid
+    if [[ ${PID_FLAG} -ne 0 ]]
+    then
+        echo "warn: $APP_NAME already started, ignoring startup request."
+    else
+        echo "starting $APP_NAME ..."
+        rm -f ${PID_FILE}
+        #rm -rf ${APP_LOG_DIR}/flumeES/*
+        ${JAVA_CMD} -jar ${JAVA_OPTS} $1 -Denv=$ENV -Dapollo.meta=${APOLLO_META_SERVER} ${APP_DEP_DIR}/${APP_NAME}
+        echo $! > ${PID_FILE}
     fi
 }
 
@@ -117,7 +186,7 @@ stop() {
 status() {
     #检查进程状态
     checktpid
-	
+
     if [[ ${PID_FLAG} -eq 0 ]]
 	  then
         echo "$APP_NAME is not runing"
@@ -138,11 +207,14 @@ case "$1" in
         stop
         start
         ;;
+    'install')
+        install
+        ;;
     'status')
         status
         ;;
     *)
-    echo "usage: $0 {start|stop|restart|status}"
+    echo "usage: $0 {start|stop|restart|install|status}"
     exit 1
     ;;
 esac

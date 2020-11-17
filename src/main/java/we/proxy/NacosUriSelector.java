93fa5f9c4607dc1125a483ec4e8bc999f80c34b0
zhongjie <ryan.zhong@163.com>
2020-11-17 10:36:48 +0800

    1.update the nacos uri selector to find the same group instance. 2.update version to 1.2.0. 3.update the readme doc.

--- src/main/java/we/proxy/NacosUriSelector.java
+++ src/main/java/we/proxy/NacosUriSelector.java
@@ -1,5 +1,7 @@
 package we.proxy;
 
+import com.alibaba.boot.nacos.discovery.properties.NacosDiscoveryProperties;
+import com.alibaba.boot.nacos.discovery.properties.Register;
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
@@ -8,6 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The Nacos implementation of {@code DiscoveryClientUriSelector}
@@ -19,8 +26,34 @@ import org.springframework.stereotype.Service;
 public class NacosUriSelector extends AbstractDiscoveryClientUriSelector {
     private static final Logger log = LoggerFactory.getLogger(NacosUriSelector.class);
 
+    public NacosUriSelector(NacosDiscoveryProperties discoveryProperties) {
+        this.discoveryProperties = discoveryProperties;
+    }
+
     @NacosInjected
     private NamingService naming;
+    private NacosDiscoveryProperties discoveryProperties;
+    private String groupName;
+    private List<String> clusterNameList;
+    private boolean useGroupName;
+    private boolean userClusterName;
+
+    @PostConstruct
+    public void init() {
+        Register register = discoveryProperties.getRegister();
+        if (register != null) {
+            this.groupName = register.getGroupName();
+            if (StringUtils.hasText(groupName)) {
+                this.useGroupName = true;
+            }
+            String clusterName = register.getClusterName();
+            if (StringUtils.hasText(clusterName)) {
+                this.userClusterName = true;
+                this.clusterNameList = Collections.singletonList(clusterName);
+            }
+
+        }
+    }
 
     @Override
     public String getNextUri(String service, String relativeUri) {
@@ -31,7 +64,15 @@ public class NacosUriSelector extends AbstractDiscoveryClientUriSelector {
     private Instance selectOneHealthyInstance(String service) {
         Instance instance = null;
         try {
-            instance = naming.selectOneHealthyInstance(service);
+            if (useGroupName && userClusterName) {
+                instance = naming.selectOneHealthyInstance(service, groupName, clusterNameList);
+            } else if (useGroupName) {
+                instance = naming.selectOneHealthyInstance(service, groupName);
+            } else if (userClusterName) {
+                instance = naming.selectOneHealthyInstance(service, clusterNameList);
+            } else {
+                instance = naming.selectOneHealthyInstance(service);
+            }
         } catch (NacosException e) {
             log.warn("Nacos selectOneHealthyInstance({}) exception", service, e);
         }

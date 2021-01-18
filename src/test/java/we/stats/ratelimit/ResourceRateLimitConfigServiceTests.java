cefb42ed1358dfd2fa048ff240d03ae842dc96e6
hongqiaowei <hongqiaowei@163.com>
2021-01-18 15:25:12 +0800

    update: roll back spring boot version to 2.2.12

--- src/test/java/we/stats/ratelimit/ResourceRateLimitConfigServiceTests.java
+++ src/test/java/we/stats/ratelimit/ResourceRateLimitConfigServiceTests.java
@@ -1,9 +1,11 @@
 package we.stats.ratelimit;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import redis.embedded.RedisServer;
 import we.redis.RedisProperties;
 import we.redis.RedisServerConfiguration;
 import we.redis.RedisTemplateConfiguration;
@@ -26,6 +28,13 @@ public class ResourceRateLimitConfigServiceTests {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+    ResourceRateLimitConfigService resourceRateLimitConfigService;
+
+    @BeforeAll
+    void beforeAllTests() {
+        resourceRateLimitConfigService = new ResourceRateLimitConfigService();
+    }
 
     @Test
     void initTest() throws InterruptedException {

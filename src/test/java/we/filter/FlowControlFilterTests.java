4398dd1afec1d0c590471840857f821ede936a83
hongqiaowei <hongqiaowei@163.com>
2021-01-19 18:57:13 +0800

    update: add flow control filter tests

--- src/test/java/we/filter/FlowControlFilterTests.java
+++ src/test/java/we/filter/FlowControlFilterTests.java
@@ -1,12 +1,23 @@
 package we.filter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebHandler;
+import reactor.core.publisher.Mono;
 import we.controller.FlowControlController;
+import we.stats.FlowStat;
+import we.stats.ratelimit.ResourceRateLimitConfig;
+import we.stats.ratelimit.ResourceRateLimitConfigService;
 import we.util.JacksonUtils;
+import we.util.ReflectionUtils;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FlowControlFilterTests {
 
     @Test
-    void test() throws InterruptedException {
+    void flowControlControllerTest() throws InterruptedException {
         WebTestClient client = WebTestClient.bindToController(new FlowControlController()).build();
         client.get().uri("/admin/flowStat/globalConcurrentsRps")
                     .accept(MediaType.APPLICATION_JSON)
@@ -32,6 +43,37 @@ public class FlowControlFilterTests {
                                                     }
                                              )
                     ;
+        Thread.sleep(3000);
+    }
+
+    @Test
+    void flowControlFilterTest() throws NoSuchFieldException, InterruptedException {
+
+        FlowControlFilter flowControlFilter = new FlowControlFilter();
+        ReflectionUtils.set(flowControlFilter, "flowControl", true);
+        FlowStat flowStat = new FlowStat();
+        ReflectionUtils.set(flowControlFilter, "flowStat", flowStat);
+
+        ResourceRateLimitConfigService resourceRateLimitConfigService = new ResourceRateLimitConfigService();
+        Map<String, ResourceRateLimitConfig> map = resourceRateLimitConfigService.getResourceRateLimitConfigMap();
+        ResourceRateLimitConfig config = JacksonUtils.readValue("{\"concurrents\":66,\"enable\":1,\"id\":1,\"isDeleted\":0,\"resource\":\"_global\",\"type\":1}", ResourceRateLimitConfig.class);
+        map.put(ResourceRateLimitConfig.GLOBAL, config);
+        ReflectionUtils.set(flowControlFilter, "resourceRateLimitConfigService", resourceRateLimitConfigService);
+
+        WebTestClient client = WebTestClient.bindToWebHandler(
+                                                    new WebHandler() {
+                                                        @Override
+                                                        public Mono<Void> handle(ServerWebExchange exchange) {
+                                                            ServerHttpResponse resp = exchange.getResponse();
+                                                            resp.setStatusCode(HttpStatus.OK);
+                                                            resp.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                                                            return resp.writeWith(Mono.just(resp.bufferFactory().wrap("{\"hello\":\"world\"}".getBytes())));
+                                                        }
+                                                    }
+                                            )
+                                            .webFilter(flowControlFilter)
+                                            .build();
+        client.get().uri("/proxy/xservice/ypath").exchange();
         Thread.sleep(3000);
     }
 }

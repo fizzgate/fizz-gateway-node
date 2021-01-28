71ed1639093adfe4ddd9913aaf74a1b0c38bac08
hongqiaowei <hongqiaowei@163.com>
2021-01-28 20:01:35 +0800

    update: push callback req to manager

--- src/main/java/we/filter/CallbackFilter.java
+++ src/main/java/we/filter/CallbackFilter.java
@@ -153,7 +153,7 @@ public class CallbackFilter extends FizzWebFilter {
         }
         return Flux.mergeSequential(monos)
                    .reduce(
-                           new ArrayList<Object>(4),
+                           new ArrayList<Object>(rs),
                            (respCollector, resp) -> {
                                respCollector.add(resp);
                                return respCollector;

bd67e365405818fd01e631911a212aa0991f118c
hongqiaowei <hongqiaowei@163.com>
2021-01-31 20:02:57 +0800

    update: add call back service

--- src/main/java/we/controller/CallbackController.java
+++ src/main/java/we/controller/CallbackController.java
@@ -17,15 +17,65 @@
 
 package we.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import we.flume.clients.log4j2appender.LogService;
+import we.proxy.CallbackReplayReq;
+import we.proxy.CallbackService;
+import we.util.Constants;
+import we.util.JacksonUtils;
+import we.util.ReactiveResult;
+
+import javax.annotation.Resource;
 
 /**
  * @author hongqiaowei
  */
 
 @RestController
-@RequestMapping(value = "/admin/callback")
+@RequestMapping(value = "/admin")
 public class CallbackController {
+
+    private static final Logger log = LoggerFactory.getLogger(CallbackController.class);
+
+    @Resource
+    private CallbackService callbackService;
+
+    @PostMapping("/callback")
+    public Mono<String> callback(ServerWebExchange exchange, @RequestBody CallbackReplayReq req) {
+
+        if (log.isDebugEnabled()) {
+            log.debug(JacksonUtils.writeValueAsString(req), LogService.BIZ_ID, req.id);
+        }
+
+        return
+        callbackService.replay(req)
+                .onErrorResume(
+                        t -> {
+                            return Mono.just(ReactiveResult.fail(t));
+                        }
+                )
+                .map(
+                        r -> {
+                            ServerHttpResponse resp = exchange.getResponse();
+                            if (r.code == ReactiveResult.SUCC) {
+                                resp.setStatusCode(HttpStatus.OK);
+                                return Constants.Symbol.EMPTY;
+                            } else {
+                                resp.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+                                return r.t.getMessage();
+                            }
+                        }
+                )
+                ;
+    }
 
 }

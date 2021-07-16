3ac7a7fc7fb138b04325ab11402091bb83e36cb4
Francis Dong <dxfeng10@gmail.com>
2021-07-16 17:13:06 +0800

    Support configuring multiple values in http header/query parameter/form-data #246

--- fizz-core/src/main/java/we/fizz/exception/FizzRuntimeException.java
+++ fizz-core/src/main/java/we/fizz/exception/FizzRuntimeException.java
@@ -1,11 +1,37 @@
 package we.fizz.exception;
 
+import we.fizz.StepContext;
+
 public class FizzRuntimeException extends RuntimeException {
+
+	private StepContext<String, Object> stepContext;
+
 	public FizzRuntimeException(String message) {
 		super(message);
 	}
 
 	public FizzRuntimeException(String message, Throwable cause) {
 		super(message, cause);
+		this.setStackTrace(cause.getStackTrace());
 	}
+
+	public FizzRuntimeException(String message, StepContext<String, Object> stepContext) {
+		super(message);
+		this.stepContext = stepContext;
+	}
+
+	public FizzRuntimeException(String message, Throwable cause, StepContext<String, Object> stepContext) {
+		super(message, cause);
+		this.setStackTrace(cause.getStackTrace());
+		this.stepContext = stepContext;
+	}
+
+	public StepContext<String, Object> getStepContext() {
+		return stepContext;
+	}
+
+	public void setStepContext(StepContext<String, Object> stepContext) {
+		this.stepContext = stepContext;
+	}
+
 }

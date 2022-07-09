7799360a80dbccb169b78579a7eab95d1769f005
hongqiaowei <hongqiaowei@163.com>
2022-07-09 11:07:15 +0800

    optimization1

--- fizz-core/src/main/java/we/plugin/stat/StatPluginFilterProperties.java
+++ fizz-core/src/main/java/we/plugin/stat/StatPluginFilterProperties.java
@@ -37,7 +37,7 @@ public class StatPluginFilterProperties {
     @Value("${stat.open:false}")
     private boolean statOpen = false;
 
-    @Value("${stat.channel:fizz_access_stat}")
+    @Value("${stat.channel:fizz_access_stat_new}")
     private String fizzAccessStatChannel;
 
     @Value("${stat.topic:}")

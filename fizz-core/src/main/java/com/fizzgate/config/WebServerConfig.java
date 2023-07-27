2726f1824a46f9c92bfb3bea6bb99da1359131de
hqw <hongqiaowei@163.com>
2023-07-27 12:14:19 +0800

    扩展webserver配置

--- fizz-core/src/main/java/com/fizzgate/config/WebServerConfig.java
+++ fizz-core/src/main/java/com/fizzgate/config/WebServerConfig.java
@@ -19,9 +19,12 @@ package com.fizzgate.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.codec.multipart.MultipartHttpMessageReader;
@@ -29,6 +32,7 @@ import org.springframework.http.codec.multipart.SynchronossPartHttpMessageReader
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import reactor.netty.http.server.HttpRequestDecoderSpec;
 
 import javax.annotation.Resource;
 
@@ -41,7 +45,7 @@ import javax.annotation.Resource;
 @EnableConfigurationProperties(ServerProperties.class)
 public class WebServerConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(WebServerConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebServerConfig.class);
     
     @Configuration
     @EnableWebFlux
@@ -68,4 +72,35 @@ public class WebServerConfig {
                     .addResourceLocations("classpath:/static/");
         }
     }
+
+    @Configuration
+    public static class FizzWebServerFactoryCustomizer implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+        @Value("${fizz.client-request.max-initial-line-length:16384}")
+        private int maxInitialLineLength;
+
+        @Value("${fizz.client-request.max-header-size:8192}")
+        private int maxHeaderSize;
+
+        @Value("${fizz.client-request.max-chunk-size:8192}")
+        private int maxChunkSize;
+
+        @Override
+        public void customize(NettyReactiveWebServerFactory factory) {
+            factory.addServerCustomizers(
+                    httpServer -> {
+                        return httpServer.httpRequestDecoder(
+                                spec -> {
+                                    HttpRequestDecoderSpec decoderSpec = spec.maxInitialLineLength(maxInitialLineLength)
+                                                                             .maxHeaderSize       (maxHeaderSize)
+                                                                             .maxChunkSize        (maxChunkSize);
+                                    LOGGER.info("set client request max initial line length to {}, max header size to {}, max chunk size to {}", maxInitialLineLength, maxHeaderSize, maxChunkSize);
+                                    return decoderSpec;
+                                }
+                        );
+                    }
+            );
+        }
+    }
+
 }
